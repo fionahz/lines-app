9cd01d321944763c1f54c9da0a3b1129bb87012e
Fiona Hall-Zazueta <fhallzaz@stanford.edu>
2018-07-09 09:50:09 -0700

    Changes to enable cue mode and click as opposed to time progression.

--- lines_app_v0.py
+++ lines_app_v0.py
@@ -5,12 +5,11 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
-from kivy.uix.popup import  Popup
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.label import Label
 from kivy.base import runTouchApp
-from kivy.properties import ObjectProperty, NumericProperty, StringProperty, ListProperty
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty, ListProperty, BooleanProperty
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
 
 from kivy.clock import Clock, mainthread
 from functools import partial
@@ -30,10 +29,14 @@ PLAY_LIST = ['miniScript.txt', 'smallerScript.txt','TEST','TEST','TEST','TEST','
 class Play(Button):
     pass
 
+class Prompt(Button):
+    pass
 
 class Start(Button):
     pass
 
+class Cues(CheckBox):
+    pass
 
 class Container(GridLayout):
     display = ObjectProperty()
@@ -41,40 +44,74 @@ class Container(GridLayout):
     userChar = StringProperty()
     playName = StringProperty('None Selected')
     lineNum = NumericProperty()
+    charLineNum = NumericProperty()
     charList = ListProperty()
     lineList = ListProperty()
     playList = ListProperty(PLAY_LIST)
     charmenu = DropDown()
     playMenu = DropDown()
+    cuesMode = BooleanProperty(False)
 
     def play(self):
 
+
+        Prompt.disabled = False
+
         def readLine():
-            if self.charList[self.lineNum] == self.userChar:
-                os.system("osascript -e 'set Volume 0'")
-            else:
-                os.system("osascript -e 'set Volume 1'")
-            
-            # Escape apostrophes:
-            lineForSpeak = self.lineList[self.lineNum].replace("\'", "\\\'")
-            os.system("say -v Alex" + lineForSpeak)
+            if not (self.charList[self.lineNum] == self.userChar):
+                lineForSpeak = self.lineList[self.lineNum].replace("\'", "\\\'")
+                os.system("say -v Alex" + lineForSpeak)
             self.lineNum += 1
-            if self.lineNum < len(self.lineList):
-                Clock.schedule_once(lambda dt: setDisplay(), 1.5)
-            else:
-                self.lineNum = 0
 
         def setDisplay():
-            self.display.text = self.lineList[self.lineNum]
-            if self.lineNum < len(self.lineList):
-                Clock.schedule_once(lambda dt: readLine(), 0.5)
-                print("lineNum = " + str(self.lineNum))
+            print("Line Num is " + str(self.lineNum))
+            if self.lineNum == 0:
+                print('BEGINNING WAS ENTERED')
+                self.display.text = 'You are at the beginning of the script.\nIf you have the first line, speak now.' 
+                if not self.charList[0] == self.userChar: 
+                    self.charLineNum = -1
+            if self.cuesMode: 
+                if self.lineNum < (len(self.lineList) - 1):
+                    print("lineNum = " + str(self.lineNum) + " and next character is " + self.charList[self.lineNum  + 1])
+                    if self.charList[self.lineNum  + 1] == self.userChar and (not self.charList[self.lineNum] == self.userChar):
+                        self.display.text = self.lineList[self.lineNum]
+                        Clock.schedule_once(lambda dt: readLine(), 0.2) 
+                        self.charLineNum = self.lineNum + 1
+                    else:
+                        print("Else was entered.")
+                        while self.lineNum < (len(self.lineList) - 1):
+                            print("In loop.")
+                            self.lineNum += 1   
+                            if self.lineNum == (len(self.lineList) - 1):
+                                setDisplay()
+                                break
+                            if self.charList[self.lineNum + 1] == self.userChar:
+                                setDisplay()
+                                break
+                else:
+                    self.lineNum = 0
+                    print('Back to beginning!')
+                    self.display.text = 'You have reached the end of this script.\nClick \'Next Cue!\' to start again'           
+                    self.charLineNum = -2
             else:
-                self.lineNum = 0
+                if self.lineNum < len(self.lineList):
+                    if self.charList[self.lineNum] == self.userChar:
+                        self.display.text = 'Your  line!'    
+                    else:
+                        self.display.text = self.lineList[self.lineNum]
+                    Clock.schedule_once(lambda dt: readLine(), 0.2)
+                else:
+                    self.lineNum = 0
+                    self.display.text = 'You have reached the end of this script.\nClick Play to start again'
 
         setDisplay()
 
     def start(self):
+
+        self.display.text = ''
+        self.lineNum = 0
+        self.charList.clear()
+        self.lineList.clear()
 
         Play.disabled = False
         self.playName = 'None Selected'
@@ -149,6 +186,23 @@ class Container(GridLayout):
         self.charmenu.bind(on_select=lambda instance, x:setUserChar(x))
         
         runTouchApp(mainbutton)
+
+        
+    def on_checkbox_active(self, checked):
+        if checked:
+            print('The checkbox is active')
+            self.cuesMode = True
+        else:
+            print('The checkbox is inactive')
+            self.cuesMode = False
+
+    def promptMe(self):
+        if self.charLineNum == -1:
+            self.display.text = 'You don\'t have the first line, so click \'Next Cue!\' to get started!'
+        elif self.charLineNum == -2:
+            self.display.text = 'You\'re at the end of the script!  Click \'Next Cue!\' to return to the beginning!'
+        else:
+            self.display.text = self.lineList[self.charLineNum]
 
 class MainApp(App):
 
