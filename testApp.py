f7839742d629bfa570b7f8bd85b45d9f998f2f39
Fiona Hall-Zazueta <fhallzaz@stanford.edu>
2018-07-22 21:05:54 -0700

    Change text file names and contents, and updated ui for testApp

--- testApp.py
+++ testApp.py
@@ -2,7 +2,8 @@ import kivy
 kivy.require('1.10.1')
 
 from kivy.app import App
-from kivy.properties import ObjectProperty, NumericProperty, StringProperty, ListProperty, BooleanProperty
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty, \
+ListProperty, BooleanProperty
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.gridlayout import GridLayout
@@ -13,7 +14,20 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.screenmanager import NoTransition 
 
 #Global Constant with all properly formatted scripts
-PLAY_LIST = ['Mini Script', 'Smaller Script','This Is A Test Title','TEST2','TEST3','TEST4','TEST5','TEST6','TEST7','TEST8','TEST9','TEST10','TEST11','TEST12','TEST13','TEST14','TEST15','TEST16','TEST17','TEST18','TEST19','TEST20','TEST21','TEST22','TEST23','TEST24','TEST25']
+PLAY_LIST = ['Mini Script', 'Smaller Script','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click', \
+'Test Button - Do Not Click','Test Button - Do Not Click']
 
 class MenuScreen(Screen):
     pass
@@ -23,32 +37,61 @@ class RehearseScreen(Screen):
 
 class TestApp(App):
 
-    playName = StringProperty()
     userChar = StringProperty()
     playName = StringProperty('None Selected')
+    
     lineNum = NumericProperty()
     charLineNum = NumericProperty()
+    
     charList = ListProperty()
     lineList = ListProperty()
-    playList = ListProperty(PLAY_LIST)
+    
     cuesMode = BooleanProperty(False)
+
+    def charSelect(self, screens):
+        
+        charMenu = screens.current_screen.ids.charMenu
+        charMenu.clear_widgets()
+        cast = []
+
+        for character in self.charList:
+            if character not in cast:
+                btn = Button(text=character, size_hint_y=None, height=44)
+
+                btn.bind(on_release=lambda btn: charMenu.select(btn.text))
+
+                charMenu.add_widget(btn)
+                cast.append(character)
+        
+        charButton = screens.current_screen.ids.charButton 
+
+        def setUserChar(selection):
+            setattr(charButton, 'text', selection)
+            if selection == '':
+                setattr(charButton, 'text', 'Character')
+            self.userChar = selection
+
+        charButton.bind(on_press=lambda x:setUserChar(''))
+
+        charMenu.bind(on_select=lambda instance, x:setUserChar(x))
 
     def build(self):
 
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(RehearseScreen(name='rehearse'))
-        #sm.transition = NoTransition(duration=0)
+        sm.transition = NoTransition(duration=0)
 
         def selectPlay(selection):
-            print(sm.screen_names)
+
+            print('play selected')
+
+            self.charList = []
+            self.lineList = []
             sm.current = 'rehearse'
-            print(sm.current)
             result =  "".join(selection.split(" ")).lower()
             self.playName = result + ".txt"
-            print(self.playName)
 
-            print('load Button pressed')
             fileObject = open(self.playName)
             
             for line in fileObject: 
@@ -58,15 +101,17 @@ class TestApp(App):
                 if len(lineArray) > 1:
                     self.charList.append(lineArray[0])
                     self.lineList.append(lineArray[1])
-        
-            print(self.charList)
-            print(self.lineList)
 
-        print(sm.current_screen.ids)
+            print(self.charList)
+            
+            self.charSelect(sm)
+
         playMenu = sm.current_screen.ids.playMenu
         playMenu.bind(minimum_height=playMenu.setter('height'))
         for i in range(len(PLAY_LIST)):
             btn = Button(text=PLAY_LIST[i], font_size=30, size_hint=(1, None))
+            if btn.text == 'Test Button - Do Not Click':
+                btn.background_color = [1, 0, 0, 1]
             btn.bind(on_release=lambda x:selectPlay(x.text))
             playMenu.add_widget(btn)
 
